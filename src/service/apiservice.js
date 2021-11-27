756589042cb40beedb77b6ac1cf1e664e064cf3b
fernando farias <fernandolol1337@gmail.com>
2021-11-27 00:10:10 -0300

    adicionando a url da api remota

--- src/service/apiservice.js
+++ src/service/apiservice.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://minhasfinancas-backend-api.herokuapp.com",
 });
 
 class ApiService {
